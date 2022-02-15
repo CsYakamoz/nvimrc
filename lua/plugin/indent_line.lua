e8c7ea98b328a2aad2b2f745ae59e5c9ccd0a395
CsYakamoz <532609418@qq.com>
2022-02-15 20:44:52 +0800

    chore: update indent-line plugin config

--- lua/plugin/indent_line.lua
+++ lua/plugin/indent_line.lua
@@ -1,9 +1,24 @@
 local indent_blankline = require("indent_blankline")
 
 indent_blankline.setup({
-	char = "▏",
+	char = "┊",
+	context_char = "│",
+	use_treesitter = true,
+	show_current_context = true,
+
 	show_end_of_line = false,
 	disable_with_nolist = true,
+
 	buftype_exclude = { "terminal" },
-	filetype_exclude = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha" },
+	filetype_exclude = {
+		"help",
+		"git",
+		"markdown",
+		"snippets",
+		"text",
+		"gitconfig",
+		"alpha",
+		"NvimTree",
+		"packer",
+	},
 })
