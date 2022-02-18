acdfc5018ccc089d25803ef5021c42faaf08d2f4
CsYakamoz <532609418@qq.com>
2022-02-18 09:37:19 +0800

    chore: update file-explorer keymap

--- lua/plugin/file-explorer.lua
+++ lua/plugin/file-explorer.lua
@@ -22,22 +22,21 @@ vim.g.nvim_tree_icons = {
 local nvim_tree = require("nvim-tree")
 
 local mapping_list = {
-	{ key = "x",         action = "close_node" },
-	{ key = "<C-r>",     action = "refresh" },
-	{ key = "C",         action = "cd" },
-	{ key = "u",         action = "dir_up" },
+	{ key = "x",     action = "close_node" },
+	{ key = "<C-r>", action = "refresh" },
+	{ key = "C",     action = "cd" },
+	{ key = "u",     action = "dir_up" },
 
-	{ key = "o",         action = "edit" },
-	{ key = "<CR>",      action = "edit" },
-	{ key = "<C-s>",     action = "split" },
-	{ key = "<C-v>",     action = "vsplit" },
-	{ key = "<C-t>",     action = "tabnew" },
+	{ key = { "o", "<CR>" },           action = "edit" },
+	{ key = { "<C-s>", "<C-w><C-s>" }, action = "split" },
+	{ key = { "<C-v>", "<C-w><C-v>" }, action = "vsplit" },
+	{ key = { "<C-t>", "<C-w><C-t>" }, action = "tabnew" },
 
-	{ key = "p",         action = "parent_node" },
-	{ key = "<C-k>",     action = "prev_sibling" },
-	{ key = "<C-j>",     action = "next_sibling" },
-	{ key = "K",         action = "first_sibling" },
-	{ key = "J",         action = "last_sibling" },
+	{ key = "p",     action = "parent_node" },
+	{ key = "<C-k>", action = "prev_sibling" },
+	{ key = "<C-j>", action = "next_sibling" },
+	{ key = "K",     action = "first_sibling" },
+	{ key = "J",     action = "last_sibling" },
 
 	{ key = "a",         action = "create" },
 	{ key = "dd",        action = "trash" },
@@ -46,16 +45,16 @@ local mapping_list = {
 	{ key = "<Leader>p", action = "paste" },
 	{ key = "r",         action = "rename" },
 
-	{ key = "y",         action = "copy_name" },
-	{ key = "Y",         action = "copy_path" },
-	{ key = "gy",        action = "copy_absolute_path" },
+	{ key = "y",  action = "copy_name" },
+	{ key = "Y",  action = "copy_path" },
+	{ key = "gy", action = "copy_absolute_path" },
 
-	{ key = "[c",        action = "prev_git_item" },
-	{ key = "]c",        action = "next_git_item" },
+	{ key = "[c", action = "prev_git_item" },
+	{ key = "]c", action = "next_git_item" },
 
-	{ key = "I",         action = "toggle_ignored" },
-	{ key = "H",         action = "toggle_dotfiles" },
-	{ key = "g?",        action = "toggle_help" },
+	{ key = "I",  action = "toggle_ignored" },
+	{ key = "H",  action = "toggle_dotfiles" },
+	{ key = "g?", action = "toggle_help" },
 
 	{ key = "<Tab>",     action = "preview" },
 	{ key = "<Leader>o", action = "system_open" },
@@ -97,7 +96,7 @@ nvim_tree.setup({
 		timeout = 400,
 	},
 	view = {
-		width = '17%',
+		width = "17%",
 		hide_root_folder = false,
 		side = "left",
 		auto_resize = true,
@@ -107,7 +106,7 @@ nvim_tree.setup({
 		},
 		number = false,
 		relativenumber = false,
-		signcolumn = 'no',
+		signcolumn = "no",
 	},
 	trash = {
 		cmd = "trash",
