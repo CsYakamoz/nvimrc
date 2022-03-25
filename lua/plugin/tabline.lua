9862b4388999b70047b9cdfb8a0a00bbba4d9478
CsYakamoz <532609418@qq.com>
2022-03-26 01:27:00 +0800

    chore: update tabline config

--- lua/plugin/tabline.lua
+++ lua/plugin/tabline.lua
@@ -3,11 +3,10 @@ local presets = require("tabby.presets")
 
 tabby.setup({
 	tabline = presets.active_wins_at_tail,
+	opt = {
+		show_at_least = 2,
+	},
 })
-
--- TODO: update after supporting the feature
--- https://github.com/nanozuki/tabby.nvim/issues/32
-vim.o.showtabline = 1
 
 local keymap = require("user.keymap")
 keymap.map("n", "<F2>", ":TabRename ", keymap.empty_opts)
