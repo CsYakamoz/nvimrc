09ea340154d7258d062ac1e0efea56938c590463
CsYakamoz <532609418@qq.com>
2022-04-20 00:43:16 +0800

    chore(treesitter): update

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,14 +1,13 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
+	ensure_installed = "all",
+	ignore_install = {}, -- List of parsers to ignore installing (for "all")
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
-		additional_vim_regex_highlighting = true,
+		enable = true,
+		disable = { "bash" },
 	},
+	-- andymass/vim-matchup: Tree-sitter integration
 	matchup = {
 		enable = true,
 	},
