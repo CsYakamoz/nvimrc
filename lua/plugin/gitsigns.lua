a1613f6c49bd1e89f4169a83c5ffd0a0745bda37
CsYakamoz <532609418@qq.com>
2022-09-12 14:02:14 +0800

    chore(gitsigns): simplify config

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -1,6 +1,4 @@
-local gs = require("gitsigns")
-
-gs.setup({
+require("gitsigns").setup({
 	signs = {
 		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
 		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -13,41 +11,13 @@ gs.setup({
 			linehl = "GitSignsChangeLn",
 		},
 	},
-	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-	watch_gitdir = {
-		interval = 1000,
-		follow_files = true,
-	},
-	attach_to_untracked = true,
 	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-	current_line_blame_opts = {
-		virt_text = true,
-		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
-		ignore_whitespace = false,
-	},
 	current_line_blame_formatter_opts = {
 		relative_time = false,
 	},
-	sign_priority = 6,
-	update_debounce = 100,
-	status_formatter = nil, -- Use default
-	max_file_length = 40000,
-	preview_config = {
-		-- Options passed to nvim_open_win
-		border = "single",
-		style = "minimal",
-		relative = "cursor",
-		row = 0,
-		col = 1,
-	},
-	yadm = {
-		enable = false,
-	},
 	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
 		local function map(mode, l, r, opts)
 			opts = opts or {}
 			opts.buffer = bufnr
@@ -55,12 +25,29 @@ gs.setup({
 		end
 
 		-- Navigation
-		map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-		map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+		map("n", "]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
 
 		-- Actions
-		map({ "n", "v" }, "<leader>hs", gs.stage_hunk)
-		map({ "n", "v" }, "<leader>hr", gs.reset_hunk)
+		map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+		map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
 		map("n", "<leader>hS", gs.stage_buffer)
 		map("n", "<leader>hu", gs.undo_stage_hunk)
 		map("n", "<leader>hR", gs.reset_buffer)
