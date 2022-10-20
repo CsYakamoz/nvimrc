dd6f723a7e6d7a0cd164784bd53bac3f49d74834
CsYakamoz <532609418@qq.com>
2022-10-21 00:20:22 +0800

    chore(treesitter): update the logic whether disable highlight

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,11 +1,26 @@
 local configs = require("nvim-treesitter.configs")
 
+local highlight_disabl_list = { "bash" }
+local function whether_disable_highlight(lang, buf)
+	for _, val in ipairs(highlight_disabl_list) do
+		if lang == val then
+			return false
+		end
+	end
+
+	local max_filesize = 100 * 1024 -- 100 KB
+	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	if ok and stats and stats.size > max_filesize then
+		return true
+	end
+end
+
 configs.setup({
 	ensure_installed = "all",
 	ignore_install = {}, -- List of parsers to ignore installing (for "all")
 	highlight = {
 		enable = true,
-		disable = { "bash" },
+		disable = whether_disable_highlight,
 	},
 	textobjects = {
 		select = {
