1bc08796fe81097c91f7b67dab649429612071cd
CsYakamoz <532609418@qq.com>
2022-10-30 20:31:39 +0800

    style: use space instead of tab

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,8 @@
 vim.cmd([[
 try
-	let g:everforest_enable_italic = 1
-	let g:everforest_better_performance = 1
-	let g:everforest_diagnostic_text_highlight = 1
+    let g:everforest_enable_italic = 1
+    let g:everforest_better_performance = 1
+    let g:everforest_diagnostic_text_highlight = 1
     colorscheme everforest
 catch /^Vim\%((\a\+)\)\=:E185/
     colorscheme default
