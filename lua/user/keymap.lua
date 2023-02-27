673bf5a0b4ccadc2afe2af4abc95bfc449f82d8b
CsYakamoz <532609418@qq.com>
2023-02-28 00:48:27 +0800

    chore(keymap): disable `<C-j>`, `<C-k>` when in insert mode

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -37,9 +37,9 @@ set("n", "Q", "<Nop>", opts)
 -- Force myself to use <C-[> in insert mode
 set("i", "<C-c>", "<Nop>", opts)
 
--- Like j,k in normal mode
--- set("i", "<C-k>", "<Up>")
--- set("i", "<C-j>", "<Down>")
+-- Deprecated: Like j,k in normal mode
+set("i", "<C-k>", "<Nop>")
+set("i", "<C-j>", "<Nop>")
 
 -- Focus the current split: https://www.reddit.com/r/vim/comments/5civsq/is_there_a_way_to_focus_the_current_split/
 set("n", "<Leader>z", ":tab split<CR>", opts)
