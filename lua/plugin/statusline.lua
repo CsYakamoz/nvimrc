6a1f292031abc5e515b2f5584c4c55e38319c8f5
CsYakamoz <532609418@qq.com>
2023-03-11 02:34:13 +0800

    chore(statusline): simplify lualine config

--- lua/plugin/statusline.lua
+++ lua/plugin/statusline.lua
@@ -8,18 +8,21 @@ local diagnostics = {
 	colored = true,
 }
 
-local function trailing_space()
-	local ignored_list = { "help", "gitcommit" }
-	local ft = vim.bo.filetype
-	for _, value in ipairs(ignored_list) do
-		if ft == value then
-			return ""
+local trailing_space = {
+	function()
+		local ignored_list = { "help", "gitcommit" }
+		local ft = vim.bo.filetype
+		for _, value in ipairs(ignored_list) do
+			if ft == value then
+				return ""
+			end
 		end
-	end
 
-	local space = vim.fn.search([[\s\+$]], "nwc")
-	return space ~= 0 and string.format("[%d]trailing", space) or ""
-end
+		local space = vim.fn.search([[\s\+$]], "nwc")
+		return space ~= 0 and string.format("[%d]trailing", space) or ""
+	end,
+	color = "DiagnosticError",
+}
 
 local diff = {
 	"diff",
@@ -29,36 +32,41 @@ local diff = {
 	end,
 }
 
+local mode = {
+	"mode",
+	fmt = function(str)
+		return str:sub(1, 1)
+	end,
+	padding = { left = 1, right = 0 },
+}
+
+local location = {
+	"location",
+	padding = { left = 0, right = 1 },
+}
+
+local function indent()
+	if vim.o.expandtab then
+		return "SW:" .. vim.o.shiftwidth
+	else
+		return "TS:" .. vim.o.tabstop
+	end
+end
+
 lualine.setup({
 	options = {
-		icons_enabled = true,
-		theme = "auto",
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-		always_divide_middle = true,
+		component_separators = "",
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = { "alpha", "NvimTree", "Outline", "coctree" },
+		},
 	},
 	sections = {
-		lualine_a = {
-			{
-				"mode",
-				fmt = function(str)
-					return str:sub(1, 1)
-				end,
-			},
-		},
-		lualine_b = { "filename", diff },
-		lualine_c = {},
-		lualine_x = { { trailing_space, color = "WarningMsg" }, diagnostics },
-		lualine_y = { "filetype", "encoding", "progress" },
-		lualine_z = { "location" },
+		lualine_a = { mode },
+		lualine_b = { "filename" },
+		lualine_c = { diff },
+		lualine_x = { trailing_space, diagnostics },
+		lualine_y = { "filetype", indent, "encoding", "progress" },
+		lualine_z = { location },
 	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
 })
