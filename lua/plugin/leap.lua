1a26d2eff6e57e98c312273c26fc399643754754
CsYakamoz <532609418@qq.com>
2023-05-26 22:11:00 +0800

    Revert "fix(leap): using s/S as trigger key"
    This reverts commit b3683d6dcfbff7717692f1f43b75dc80d918b91c.

--- lua/plugin/leap.lua
+++ lua/plugin/leap.lua
@@ -2,14 +2,14 @@ local keymap = require("user.keymap")
 
 keymap.set(
 	{ "n", "v", "o" },
-	"s",
+	",",
 	"<Plug>(leap-forward-to)",
 	keymap.silent_opts("Leap Forward To")
 )
 
 keymap.set(
 	{ "n", "v", "o" },
-	"S",
+	"<leader>,",
 	"<Plug>(leap-backward-to)",
 	keymap.silent_opts("Leap Backward To")
 )
